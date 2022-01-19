f44a2dfb7ca5ae7426cf35cb6ac9d55b2920300f
asemelkhouli20 <asemelkhouli209@gmail.com>
2022-01-20 01:56:20 +0200

    Complete add trip controller

--- Itinerary/Controller/AddTripViewController.swift
+++ Itinerary/Controller/AddTripViewController.swift
@@ -12,10 +12,9 @@ import Photos
 class AddTripViewController : UIViewController {
     
     @IBOutlet weak var addimageButton: UIButton!
-    
+    @IBOutlet weak var tripName: UILabel!
     @IBOutlet weak var coverImage: UIImageView!
     @IBOutlet weak var textFiled: UITextField!
-    
     
     var newTrip : Trips!
     var tripImage:UIImage?
@@ -23,13 +22,33 @@ class AddTripViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        textFiled.delegate=self
         coverImage.layer.cornerRadius=20
-        
     }
+    
     @IBAction func cancelPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+   
+    @IBAction func addImage(_ sender: UIButton) {
+        askTheUserToChooseTheSource()
+    }
     
+    @IBAction func addPressed(_ sender: UIButton) {
+        //make sure text filed not empty and change the text filrd to alert the user
+        guard textFiled.text != "", let newTripTitle = textFiled.text else {  textFilrdIsEmpty(); return }
+        //save new trip
+        newTrip=Trips(id: UUID(), name: newTripTitle, tripImage: tripImage)
+        //back to main view and pass the data
+        if passData != nil{ passData!()  }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    //MARK: - Help methods
+    
+    //make sure the source are available and make request Authorization and present the picker if user authorized
     func imagePicker(isSourcPhotoType:UIImagePickerController.SourceType) {
         if UIImagePickerController.isSourceTypeAvailable(isSourcPhotoType){
             PHPhotoLibrary.requestAuthorization(for: .readWrite) { (statue) in
@@ -38,46 +57,19 @@ class AddTripViewController : UIViewController {
                     //open it in main threed
                     DispatchQueue.main.sync {
                         let myPickerImage=UIImagePickerController()
+                        myPickerImage.delegate=self
+                        
                         self.present(myPickerImage, animated: true, completion: nil)
                     }
-                default: break
+                default:
+                    self.makeAlert(title: " privacy error", message: "please allow to access to your resourse to take your image and make it to background to make your trip more Beautiful")
+                    break
                 }//end switch
             }//END PHPhotoLibrary
         }else{
             makeAlert(title: "source not available", message: "soory but your device doesn't have this resource")
         }
     }
-    
-    @IBAction func addImage(_ sender: UIButton) {
-        //add image; open image picker to select or take photo from camera
-        let imagePuckerSheet = UIAlertController(title: "slect image", message: "", preferredStyle: .actionSheet)
-        let cmeraAction = UIAlertAction(title: "Camera", style: .default) { action in
-            self.imagePicker(isSourcPhotoType: .camera)
-        }
-        let photoAction = UIAlertAction(title: "Photo Library", style: .default) { action in
-            self.imagePicker(isSourcPhotoType: .photoLibrary)
-        }
-        imagePuckerSheet.addAction(cmeraAction)
-        imagePuckerSheet.addAction(photoAction)
-        imagePuckerSheet.view.tintColor=UIColor.red
-        present(imagePuckerSheet, animated: true, completion: nil)
-    }
-    
-    @IBAction func addPressed(_ sender: UIButton) {
-        //make sure text filed not empty and change the text filrd to alert the user
-        guard textFiled.text != "", let newTripTitle = textFiled.text else {
-            textFiled.layer.borderColor=UIColor.red.cgColor
-            textFiled.layer.borderWidth=1
-            textFiled.layer.cornerRadius=5
-            textFiled.placeholder="title trip required"
-            return
-        }
-        //save new trip and back to main view
-        newTrip=Trips(id: UUID(), name: newTripTitle, tripImage: tripImage)
-        if passData != nil{ passData!()  }
-        self.dismiss(animated: true, completion: nil)
-    }
-    
     //make alert method
     func makeAlert(title:String,message:String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -88,4 +80,73 @@ class AddTripViewController : UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    //make red border text filed to tell user this filed is required
+    func textFilrdIsEmpty() {
+        //make warning to user
+        textFiled.layer.borderColor=UIColor.red.cgColor
+        textFiled.layer.borderWidth=1
+        textFiled.layer.cornerRadius=5
+        textFiled.placeholder="title trip required"
+    }
+    //present to user sheet and make the user to choose between the resource Camera/Photo Library
+    func askTheUserToChooseTheSource() {
+        //add image; open image picker to select or take photo from camera
+        let imagePuckerSheet = UIAlertController(title: "Slect Source", message: "select the source that you want to take image from it", preferredStyle: .actionSheet)
+        //make the action
+        let cmeraAction = UIAlertAction(title: "Camera", style: .default) { action in  self.imagePicker(isSourcPhotoType: .camera) }
+        let photoAction = UIAlertAction(title: "Photo Library", style: .default) { action in  self.imagePicker(isSourcPhotoType: .photoLibrary) }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel,handler: nil)
+        //add the action
+        imagePuckerSheet.addAction(cmeraAction)
+        imagePuckerSheet.addAction(photoAction)
+        imagePuckerSheet.addAction(cancelAction)
+        //change the tint Color to red ‏because blue is default
+        imagePuckerSheet.view.tintColor=UIColor.red
+        //present the sheet
+        present(imagePuckerSheet, animated: true, completion: nil)
+    }
+    
+}
+
+//MARK: - UIImagePickerControllerDelegate , UINavigationControllerDelegate
+extension AddTripViewController : UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    func updateCover() {
+        //remove button from show
+        coverImage.backgroundColor=UIColor.clear
+        addimageButton.tintColor=UIColor.clear
+        //update lable
+        tripName.text=textFiled.text
+        tripName.textColor=UIColor.white
+        tripName.backgroundColor=UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPicerImage = info[.originalImage] as? UIImage {
+            coverImage.image = userPicerImage
+            self.tripImage=userPicerImage
+            //make lable update
+            updateCover()
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+}
+//MARK: - UITextFieldDelegate
+extension AddTripViewController : UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateCover()
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField)->Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textFilrdIsEmpty()
+            return false
+        }
+    }
 }
--- Itinerary/Controller/TripsTableViewCell.swift
+++ Itinerary/Controller/TripsTableViewCell.swift
@@ -11,12 +11,16 @@ class TripsTableViewCell: UITableViewCell {
 
     @IBOutlet weak var title: UILabel!
     
+    @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var backgroundImage: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        backgroundImage.layer.cornerRadius=20
+        cellView.layer.cornerRadius = 20
+        title.backgroundColor=UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+        title.layer.cornerRadius=20
+        title.textColor=UIColor.white
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
